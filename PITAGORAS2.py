98d93b7fd60aa2c408a752056cf35e898e85d2f2
juliapopielarz <juliapopielarz06@gmail.com>
2025-03-31 10:02:30 +0200

    Add files via upload

---
+++ PITAGORAS2.py
@@ -0,0 +1,12 @@
+
+import streamlit as st
+st.title("Kalkulator Pitagorasa (pokazcie Celince)")
+
+a = st.number_input("Podaj długość boku a:")
+b = st.number_input("Podaj długość boku b:")
+
+#c is equal
+
+c = (a**2 + b**2)**0.5
+
+st.write(c)
